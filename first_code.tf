2d1e239a888d49ac507b4dc792a49858363d1a37
sandeep <SandeepReddy.Vangala@hilton.com>
2020-06-06 18:48:00 -0500

    added build config for 2 ec2 instances

--- first_code.tf
+++ first_code.tf
@@ -1,9 +1,23 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_s3_bucket" "first-terraform-bucket" {
   bucket = "terraform-first-bucket"
-  acl = "private"
+  acl    = "private"
+}
+
+resource "aws_instance" "first-ec2-instance" {
+  count             = 2
+  ami               = "ami-0e781a2535d5b2d02"
+  instance_type     = "t2.micro"
+  key_name          = "terraform-ec2"
+
+  tags = {
+    Name = "terraform"
+    }
+ 
+
+
 }
