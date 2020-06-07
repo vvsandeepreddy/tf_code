4165607f85693047a198e63c7e000646127f28fd
sandeep <SandeepReddy.Vangala@hilton.com>
2020-06-07 01:11:37 -0500

    added creation of eips and dependency with s3 for ec2

--- first_code.tf
+++ first_code.tf
@@ -13,11 +13,15 @@ resource "aws_instance" "first-ec2-instance" {
   ami               = "ami-0e781a2535d5b2d02"
   instance_type     = "t2.micro"
   key_name          = "terraform-ec2"
-
-  tags = {
+  depends_on        = [aws_s3_bucket.first-terraform-bucket]
+tags = {
     Name = "terraform"
-    }
- 
+}
+}
 
+resource "aws_eip" "first-eip" {
+  count     = 2
+  instance  = aws_instance.first-ec2-instance.*.id[count.index]
+  vpc       = true
 
 }
